1a90b4dc489e976920116f6c1207aa271ad0cfec
Arsenii Lyashenko <arsenylyashenko.3@gmail.com>
2021-08-13 18:26:26 +0300

    Use html2md's image handler because it's exported now

--- src/parser/tags.rs
+++ src/parser/tags.rs
@@ -1,6 +1,5 @@
 use crate::parser::make_url_from_fragment;
 use html2md::{common::get_tag_attr, Handle, StructuredPrinter, TagHandler, TagHandlerFactory};
-use percent_encoding::{utf8_percent_encode, AsciiSet, CONTROLS};
 use std::collections::HashMap;
 
 pub(crate) enum TagsHandlerFactory {
@@ -57,62 +56,12 @@ struct ImageHandler;
 
 impl TagHandler for ImageHandler {
     fn handle(&mut self, tag: &Handle, printer: &mut StructuredPrinter) {
-        // almost all of code taken from html2md source code
-        // because html2md::ImgHandler is not public
-
-        const FRAGMENT: &AsciiSet = &CONTROLS.add(b' ').add(b'"').add(b'<').add(b'>').add(b'`');
-
-        // try to extract attrs
-        let src = get_tag_attr(tag, "src");
         let alt = get_tag_attr(tag, "alt");
-        let title = get_tag_attr(tag, "title");
-        let height = get_tag_attr(tag, "height");
-        let width = get_tag_attr(tag, "width");
-        let align = get_tag_attr(tag, "align");
 
         if let Some(alt) = alt {
             printer.append_str(&alt);
-            return;
-        }
-
-        if height.is_some() || width.is_some() || align.is_some() {
-            // need to handle it as inline html to preserve attributes we support
-            printer.append_str(&format!(
-                "<img{} />",
-                alt.map(|value| format!(" alt=\"{}\"", value))
-                    .unwrap_or_default()
-                    + &src
-                        .map(|value| format!(" src=\"{}\"", value))
-                        .unwrap_or_default()
-                    + &title
-                        .map(|value| format!(" title=\"{}\"", value))
-                        .unwrap_or_default()
-                    + &height
-                        .map(|value| format!(" height=\"{}\"", value))
-                        .unwrap_or_default()
-                    + &width
-                        .map(|value| format!(" width=\"{}\"", value))
-                        .unwrap_or_default()
-                    + &align
-                        .map(|value| format!(" align=\"{}\"", value))
-                        .unwrap_or_default()
-            ));
         } else {
-            // need to escape URL if it contains spaces
-            // don't have any geometry-controlling attrs, post markdown natively
-            let mut img_url = src.unwrap_or_default();
-            if img_url.contains(' ') {
-                img_url = utf8_percent_encode(&img_url, FRAGMENT).to_string();
-            }
-
-            printer.append_str(&format!(
-                "![{}]({}{})",
-                alt.unwrap_or_default(),
-                &img_url,
-                title
-                    .map(|value| format!(" \"{}\"", value))
-                    .unwrap_or_default()
-            ));
+            html2md::images::ImgHandler::default().handle(tag, printer)
         }
     }
 
